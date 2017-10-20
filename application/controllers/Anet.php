3114b2e7e47992f6cefbccbd40fc53d5f079a938
Syed Tausif <engr.tausif@yahoo.com>
2017-10-20 13:07:47 -0700

    Run CC authorization void transaction
    Signed-off-by: Syed Tausif <engr.tausif@yahoo.com>

--- application/controllers/Anet.php
+++ application/controllers/Anet.php
@@ -3,28 +3,45 @@
 class Anet extends karmora {
 
     private $authObj;
+
     public function __construct() {
         parent::__construct();
         $this->load->library('authorizenet');
         $this->authObj = new authorizenet;
+        $this->authObj->activateSandboxMode(TRUE);
     }
 
     public function index() {
-        $this->authObj->activateSandboxMode(TRUE);
         echo '<pre>';
-//        var_dump($this->authObj->chargeCC(199));
-        $this->authObj->creditCard['card_number'] = '4111111111111111';
-        $this->authObj->creditCard['exp_date'] = '2038-12';
-        $this->authObj->creditCard['cvc_code'] = '123';
+        $this->authObj->creditCardInfo['card_number'] = '4111111111111111';
+        $this->authObj->creditCardInfo['exp_date'] = '2038-12';
+        $this->authObj->creditCardInfo['cvc_code'] = '123';
+
         $this->authObj->amountToProcess = rand(10, 20);
-        var_dump($this->authObj->runAuthorization());
+        
+        var_dump($this->authObj->arbCreate());
         echo '</pre>';
         exit;
     }
-    
-    public function arbCreate() {
+
+    public function voidTransaction() {
+
         echo '<pre>';
-        var_dump($this->authObj->arbCreate());
+        var_dump($this->authObj->voidTransaction('60032345282'));
+        echo '</pre>';
+        exit;
+    }
+
+    public function runAuthorization() {
+        echo '<pre>';
+
+        $this->authObj->creditCardInfo['card_number'] = '4111111111111111';
+        $this->authObj->creditCardInfo['exp_date'] = '2038-12';
+        $this->authObj->creditCardInfo['cvc_code'] = '123';
+
+        $this->authObj->amountToProcess = rand(10, 20);
+        $this->authObj->amountTrial = 0;
+        var_dump($this->authObj->runAuthorization());
         echo '</pre>';
         exit;
     }
--- application/libraries/authorizenet.php
+++ application/libraries/authorizenet.php
@@ -17,38 +17,101 @@ class authorizenet {
     public $MERCHANT_LOGIN_ID;
     public $MERCHANT_TRANSACTION_KEY;
     public $sandBox;
-    
 //    info variables
-    
-    public $creditCard = array(
+
+    public $amountToProcess;
+    public $amountTrial;
+    public $creditCardInfo = array(
         'card_number',
         'exp_date',
         'cvc_code'
     );
-    
-    public $amountToProcess;
+    public $billingAddressInfo = array(
+        'firstName',
+        'lastName',
+        'company',
+        'address',
+        'city',
+        'state',
+        'zip',
+        'country'
+    );
+    public $shippingAddressInfo = array(
+        'firstName',
+        'lastName',
+        'company',
+        'address',
+        'city',
+        'state',
+        'zip',
+        'country'
+    );
+    public $subscriptionInfo = array(
+        'title',
+        'Descp',
+        'paymentSchedule',
+        'amount',
+        'trialAmount',
+        'startDate', //format YYYY-MM-DD
+        'intervalLength',
+        'intervalUnit',
+        'totalOccrence',
+        'trialOccrence',
+        'payment',
+        'order',
+        'customer',
+        'billTo',
+        'shipTo',
+        'profile'
+    );
+//    Auth Variables
+    private $transactionType = array(
+        'authorize' => 'authOnlyTransaction',
+        'voidTransaction' => 'voidTransaction',
+    );
+    private $authBillTo;
+    private $authCreditCard;
+    private $authInterval;
+    private $authOrder;
+    private $authPayment;
+    private $authPaymentSchedule;
+    private $authRequest;
+    private $authShipTo;
+    private $authSubscription;
+    private $authTransactionRequestType;
 
     public function __construct() {
 
         $this->activateSandboxMode();
-
+        $this->setEnvoirnment();
         $this->refId = 'ref' . time();
-        $this->sandBox ? $this->environment = APIconstant\ANetEnvironment::SANDBOX : $this->environment = APIconstant\ANetEnvironment::PRODUCTION;
+        $this->setMerchantLogin();
+    }
 
+    private function setEnvoirnment() {
+        !$this->sandBox ?
+                        $this->environment = APIconstant\ANetEnvironment::PRODUCTION :
+                        $this->environment = APIconstant\ANetEnvironment::SANDBOX;
+        return;
+    }
+
+    private function setMerchantLogin() {
         switch ($this->environment) {
-            case APIconstant\ANetEnvironment::SANDBOX:
-                $this->MERCHANT_LOGIN_ID = "3wX3X2gs2b";
-                $this->MERCHANT_TRANSACTION_KEY = "3D9KntkY3SM29t36";
-                break;
             case APIconstant\ANetEnvironment::PRODUCTION:
                 $this->MERCHANT_LOGIN_ID = '';
                 $this->MERCHANT_TRANSACTION_KEY = '';
+                break;
+            case APIconstant\ANetEnvironment::SANDBOX :
+            default :
+                $this->MERCHANT_LOGIN_ID = "3wX3X2gs2b";
+                $this->MERCHANT_TRANSACTION_KEY = "3D9KntkY3SM29t36";
                 break;
         }
 
         $this->merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
         $this->merchantAuthentication->setName($this->MERCHANT_LOGIN_ID);
         $this->merchantAuthentication->setTransactionKey($this->MERCHANT_TRANSACTION_KEY);
+        return;
     }
 
     public function activateSandboxMode($mode = TRUE) {
@@ -56,142 +119,173 @@ class authorizenet {
         return;
     }
 
-    public function arbCreate() {
-        $intervalLength = 1;
-        // Subscription Type Info
-        $subscription = new AnetAPI\ARBSubscriptionType();
-        $subscription->setName("Sample Subscription");
+    public function voidTransaction($transactionId) {
+        //create a transaction
+        $this->authTransactionRequestType = new AnetAPI\TransactionRequestType();
+        $this->authTransactionRequestType->setTransactionType($this->transactionType['voidTransaction']);
+        $this->authTransactionRequestType->setRefTransId($transactionId);
 
-        $interval = new AnetAPI\PaymentScheduleType\IntervalAType();
-        $interval->setLength($intervalLength);
-        $interval->setUnit("months");
+        $this->setRequest();
 
-        $paymentSchedule = new AnetAPI\PaymentScheduleType();
-        $paymentSchedule->setInterval($interval);
-        $paymentSchedule->setStartDate(new DateTime('2017-10-05'));
-        $paymentSchedule->setTotalOccurrences("12");
-        $paymentSchedule->setTrialOccurrences("1");
-
-        $subscription->setPaymentSchedule($paymentSchedule);
-        $subscription->setAmount(rand(1, 99999) / 12.0 * 12);
-        $subscription->setTrialAmount("0.00");
-
-        $creditCard = new AnetAPI\CreditCardType();
-        $creditCard->setCardNumber("4111111111111111");
-        $creditCard->setExpirationDate("2038-12");
-
-        $payment = new AnetAPI\PaymentType();
-        $payment->setCreditCard($creditCard);
-        $subscription->setPayment($payment);
-
-        $order = new AnetAPI\OrderType();
-        $order->setInvoiceNumber("1234354");
-        $order->setDescription("Description of the subscription");
-        $subscription->setOrder($order);
-
-        $billTo = new AnetAPI\NameAndAddressType();
-        $billTo->setFirstName("John");
-        $billTo->setLastName("Smith");
-
-        $subscription->setBillTo($billTo);
-
-        $request = new AnetAPI\ARBCreateSubscriptionRequest();
-        $request->setmerchantAuthentication($this->merchantAuthentication);
-        $request->setRefId($this->refId);
-        $request->setSubscription($subscription);
-        $controller = new AnetController\ARBCreateSubscriptionController($request);
-
+        $controller = new AnetController\CreateTransactionController($this->authRequest);
         $response = $controller->executeWithApiResponse($this->environment);
 
-        if (($response != null) && ($response->getMessages()->getResultCode() == "Ok")) {
-            echo "SUCCESS: Subscription ID : " . $response->getSubscriptionId() . "\n";
-        } else {
-            echo "ERROR :  Invalid response\n";
-            $errorMessages = $response->getMessages()->getMessage();
-            echo "Response : " . $errorMessages[0]->getCode() . "  " . $errorMessages[0]->getText() . "\n";
-        }
-
-        return $response;
+        return $this->responseCCTransaction($response);
     }
 
     public function runAuthorization() {
         // Create the payment data for a credit card
-        $creditCard = new AnetAPI\CreditCardType();
-        $creditCard->setCardNumber($this->creditCard['card_number']);
-        $creditCard->setExpirationDate($this->creditCard['exp_date']);
-        $creditCard->setCardCode($this->creditCard['cvc_code']);
+        $this->setCreditCard();
 
         // Add the payment data to a paymentType object
-        $paymentOne = new AnetAPI\PaymentType();
-        $paymentOne->setCreditCard($creditCard);
-        /*
-         * not required
+        $this->setPayment();
 
-          // Create order information
-          $order = new AnetAPI\OrderType();
-          $order->setInvoiceNumber("10101");
-          $order->setDescription("Golf Shirts");
-
-          // Set the customer's Bill To address
-          $customerAddress = new AnetAPI\CustomerAddressType();
-          $customerAddress->setFirstName("Ellen");
-          $customerAddress->setLastName("Johnson");
-          $customerAddress->setCompany("Souveniropolis");
-          $customerAddress->setAddress("14 Main Street");
-          $customerAddress->setCity("Pecan Springs");
-          $customerAddress->setState("TX");
-          $customerAddress->setZip("44628");
-          $customerAddress->setCountry("USA");
-
-          // Set the customer's identifying information
-          $customerData = new AnetAPI\CustomerDataType();
-          $customerData->setType("individual");
-          $customerData->setId("99999456654");
-          $customerData->setEmail("EllenJohnson@example.com");
-
-          // Add values for transaction settings
-          $duplicateWindowSetting = new AnetAPI\SettingType();
-          $duplicateWindowSetting->setSettingName("duplicateWindow");
-          $duplicateWindowSetting->setSettingValue("60");
-
-          // Add some merchant defined fields. These fields won't be stored with the transaction,
-          // but will be echoed back in the response.
-          $merchantDefinedField1 = new AnetAPI\UserFieldType();
-          $merchantDefinedField1->setName("customerLoyaltyNum");
-          $merchantDefinedField1->setValue("1128836273");
-
-          $merchantDefinedField2 = new AnetAPI\UserFieldType();
-          $merchantDefinedField2->setName("favoriteColor");
-          $merchantDefinedField2->setValue("blue");
-         */
         // Create a TransactionRequestType object and add the previous objects to it
-        $transactionRequestType = new AnetAPI\TransactionRequestType();
-        $transactionRequestType->setTransactionType("authOnlyTransaction");
-        $transactionRequestType->setAmount($this->amountToProcess);
-//        $transactionRequestType->setOrder($order);
-        $transactionRequestType->setPayment($paymentOne);
-//        $transactionRequestType->setBillTo($customerAddress);
-//        $transactionRequestType->setCustomer($customerData);
-//        $transactionRequestType->addToTransactionSettings($duplicateWindowSetting);
-//        $transactionRequestType->addToUserFields($merchantDefinedField1);
-//        $transactionRequestType->addToUserFields($merchantDefinedField2);
+        $this->setTransationType($this->transactionType['authorize']);
+
         // Assemble the complete transaction request
-        $request = new AnetAPI\CreateTransactionRequest();
-        $request->setMerchantAuthentication($this->merchantAuthentication);
-        $request->setRefId($this->refId);
-        $request->setTransactionRequest($transactionRequestType);
+        $this->setRequest();
 
         // Create the controller and get the response
-        $controller = new AnetController\CreateTransactionController($request);
+        $controller = new AnetController\CreateTransactionController($this->authRequest);
+        $authResponse = $controller->executeWithApiResponse($this->environment);
+
+        return $this->responseCCTransaction($authResponse);
+        ;
+    }
+
+    public function arbCreate() {
+
+        $this->setInterval();
+
+        $this->setPaymentSchedule();
+
+        $this->setCreditCard();
+
+        $this->setPayment();
+
+        $this->authBillTo = $this->setAddress($this->billingAddressInfo);
+
+        $this->authOrder = new AnetAPI\OrderType();
+        $this->authOrder->setInvoiceNumber($this->refId);
+        $this->authOrder->setDescription($this->subscriptionInfo['descp']);
+
+        // Subscription Type Info
+        $this->setSubscription();
+
+        $this->setSubscriptionRequest();
+        $controller = new AnetController\ARBCreateSubscriptionController($this->authRequest);
         $response = $controller->executeWithApiResponse($this->environment);
 
+        if (($response != null) && ($response->getMessages()->getResultCode() == APIconstant\ANetEnvironment::RESPONSE_OK)) {
+            $returnResponse = array(
+                'transaction_status' => TRUE,
+                'subscription_id' => $response->getSubscriptionId(),
+                'ref_id' => $response->getRefId(),
+                'description' => $response->getMessages(),
+            );
+        } else {
+            $errorMessages = $response->getMessages()->getMessage();
+            $returnResponse = array(
+                'transaction_status' => FALSE,
+                'error_code' => $errorMessages[0]->getCode(),
+                'error_message' => $errorMessages[0]->getText()
+            );
+        }
 
-        if ($response != null) {
+        return $returnResponse;
+    }
+
+    private function setSubscription() {
+        $this->authSubscription = new AnetAPI\ARBSubscriptionType();
+        $this->authSubscription->setName($this->subscriptionInfo['title']);
+        $this->authSubscription->setPaymentSchedule($this->authPaymentSchedule);
+        $this->authSubscription->setAmount($this->amountToProcess);
+        $this->authSubscription->setTrialAmount($this->amountTrial);
+        $this->authSubscription->setPayment($this->authPayment);
+        $this->authSubscription->setOrder($this->authOrder);
+        $this->authSubscription->setBillTo($this->authBillTo);
+        return;
+    }
+
+    private function setPaymentSchedule() {
+        $this->authPaymentSchedule = new AnetAPI\PaymentScheduleType();
+        $this->authPaymentSchedule->setInterval($this->authInterval);
+        $this->authPaymentSchedule->setStartDate(new DateTime($this->subscriptionInfo['startDate']));
+        $this->authPaymentSchedule->setTotalOccurrences($this->subscriptionInfo['totalOccurence']);
+        $this->authPaymentSchedule->setTrialOccurrences($this->subscriptionInfo['trialOccurence']);
+        return;
+    }
+
+    private function setInterval() {
+        $this->authInterval = new AnetAPI\PaymentScheduleType\IntervalAType();
+        $this->authInterval->setLength($this->subscriptionInfo['intervalLength']);
+        $this->authInterval->setUnit($this->subscriptionInfo['intervalUnit']);
+        return;
+    }
+
+    private function setCreditCard() {
+        $this->authCreditCard = new AnetAPI\CreditCardType();
+        $this->authCreditCard->setCardNumber($this->creditCardInfo['card_number']);
+        $this->authCreditCard->setExpirationDate($this->creditCardInfo['exp_date']);
+        $this->authCreditCard->setCardCode($this->creditCardInfo['cvc_code']);
+        return;
+    }
+
+    private function setPayment() {
+        $this->authPayment = new AnetAPI\PaymentType();
+        $this->authPayment->setCreditCard($this->authCreditCard);
+        return;
+    }
+
+    private function setTransationType($transactionType) {
+        $this->authTransactionRequestType = new AnetAPI\TransactionRequestType();
+        $this->authTransactionRequestType->setTransactionType($transactionType);
+        $this->authTransactionRequestType->setAmount($this->amountToProcess);
+        $this->authTransactionRequestType->setPayment($this->authPayment);
+        return;
+    }
+
+    private function setSubscriptionRequest() {
+        $this->authRequest = new AnetAPI\ARBCreateSubscriptionRequest();
+        $this->setMerchantAuth();
+        $this->authRequest->setSubscription($this->authSubscription);
+        return;
+    }
+
+    private function setRequest() {
+        $this->authRequest = new AnetAPI\CreateTransactionRequest();
+        $this->setMerchantAuth();
+        $this->authRequest->setTransactionRequest($this->authTransactionRequestType);
+        return;
+    }
+
+    private function setMerchantAuth() {
+        $this->authRequest->setMerchantAuthentication($this->merchantAuthentication);
+        $this->authRequest->setRefId($this->refId);
+        return;
+    }
+
+    private function setAddress($addressInfo) {
+        $address = new AnetAPI\NameAndAddressType();
+        $address->setFirstName($addressInfo['firstName']);
+        $address->setLastName($addressInfo['lastName']);
+        $address->company($addressInfo['company']);
+        $address->setAddress($addressInfo['address']);
+        $address->setCity($addressInfo['city']);
+        $address->setState($addressInfo['state']);
+        $address->setZip($addressInfo['zip']);
+        $address->setCountry($addressInfo['country']);
+        return $address;
+    }
+
+    private function responseCCTransaction($authResponse) {
+        if ($authResponse != null) {
             // Check to see if the API request was successfully received and acted upon
-            if ($response->getMessages()->getResultCode() == APIconstant\ANetEnvironment::RESPONSE_OK) {
+            if ($authResponse->getMessages()->getResultCode() == APIconstant\ANetEnvironment::RESPONSE_OK) {
                 // Since the API request was successful, look for a transaction response
                 // and parse it to display the results of authorizing the card
-                $tresponse = $response->getTransactionResponse();
+                $tresponse = $authResponse->getTransactionResponse();
 
                 if ($tresponse != null && $tresponse->getMessages() != null) {
                     $returnResponse = array(
@@ -203,18 +297,22 @@ class authorizenet {
                         'auth_code' => $tresponse->getAuthCode(),
                         'description' => $tresponse->getMessages()[0]->getDescription()
                     );
+                } elseif ($tresponse->getErrors() != null) {
+                    $returnResponse = array(
+                        'transaction_status' => FALSE,
+                        'error_code' => $tresponse->getErrors()[0]->getErrorCode(),
+                        'error_message' => $tresponse->getErrors()[0]->getErrorText()
+                    );
                 } else {
-                    if ($tresponse->getErrors() != null) {
-                        $returnResponse = array(
-                            'transaction_status' => FALSE,
-                            'error_code' => $tresponse->getErrors()[0]->getErrorCode(),
-                            'error_message' => $tresponse->getErrors()[0]->getErrorText()
-                        );
-                    }
+                    $returnResponse = array(
+                        'transaction_status' => FALSE,
+                        'error_code' => '----',
+                        'error_message' => 'Unknow Error.'
+                    );
                 }
                 // Or, print errors if the API request wasn't successful
             } else {
-                $tresponse = $response->getTransactionResponse();
+                $tresponse = $authResponse->getTransactionResponse();
 
                 if ($tresponse != null && $tresponse->getErrors() != null) {
                     $returnResponse = array(
@@ -225,8 +323,8 @@ class authorizenet {
                 } else {
                     $returnResponse = array(
                         'transaction_status' => FALSE,
-                        'error_code' => $response->getMessages()->getMessage()[0]->getCode(),
-                        'error_message' => $response->getMessages()->getMessage()[0]->getText()
+                        'error_code' => $authResponse->getMessages()->getMessage()[0]->getCode(),
+                        'error_message' => $authResponse->getMessages()->getMessage()[0]->getText()
                     );
                 }
             }
@@ -237,6 +335,7 @@ class authorizenet {
                 'error_message' => 'No response returned'
             );
         }
+
         return $returnResponse;
     }
 
